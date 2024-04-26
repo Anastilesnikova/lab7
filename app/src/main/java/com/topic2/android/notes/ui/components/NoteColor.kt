e9b5fb2c1f7cc8098e4229fdd8f6bcf0029a80c8
Anastilesnikova <anastacialesnikowa@gmail.com>
2024-04-27 00:43:55 +0300

    Улучшение удобства использования NoteColor

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -13,13 +13,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor() {
+fun NoteColor(
+    modifier: Modifier = Modifier,
+    color: Color,
+    size: Dp,
+    padding: Dp = 0.dp,
+    border: Dp
+) {
     Box(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(padding)
             .size(40.dp)
             .clip(CircleShape)
             .background(Color.Red)
@@ -32,11 +39,9 @@ fun NoteColor() {
             )
     )
 }
-    )
-}
 
 @Preview
 @Composable
 fun NoteColorPreview() {
-    NoteColor()
+    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
 }
