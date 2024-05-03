b904c0cafdeaf2832804ee4184628e25c18c2904
Anastilesnikova <anastacialesnikowa@gmail.com>
2024-05-04 00:53:05 +0300

    Создание панели управления

--- app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/topic2/android/notes/viewmodel/MainViewModel.kt
@@ -2,8 +2,11 @@ package com.topic2.android.notes.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.topic2.android.notes.data.repository.Repository
 import com.topic2.android.notes.domain.model.NoteModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Модель просмотра, используемая для хранения глобального состояния приложения.
