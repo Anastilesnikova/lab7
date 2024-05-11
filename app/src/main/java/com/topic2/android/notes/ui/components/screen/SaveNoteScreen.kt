4c3ec2a314f7cace8325f5f478d3957b4818c2d0
Anastilesnokova <anastacialesnikowa@gmail.com>
2024-05-11 18:08:06 +0300

    Добавление точки входа

--- app/src/main/java/com/topic2/android/notes/ui/components/screen/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screen/SaveNoteScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,6 +19,12 @@ import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.domain.model.ColorModel
 import com.topic2.android.notes.ui.components.NoteColor
 import com.topic2.android.notes.util.fromHex
+import com.topic2.android.notes.viewmodel.MainViewModel
+
+@Composable
+fun SaveNoteScreen(viewModel: MainViewModel) {
+
+}
 
 @Composable
 private fun ColorPicker(
