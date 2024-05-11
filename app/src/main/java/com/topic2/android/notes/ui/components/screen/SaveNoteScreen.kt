c1fa95302fa884c5b643aa6c3e4888347a06bf30
Anastilesnokova <anastacialesnikowa@gmail.com>
2024-05-11 18:19:22 +0300

    Добавление SaveNoteTopAppBar

--- app/src/main/java/com/topic2/android/notes/ui/components/screen/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screen/SaveNoteScreen.kt
@@ -26,6 +26,21 @@ fun SaveNoteScreen(viewModel: MainViewModel) {
 
 }
 
+
+
+@Composable
+private fun SaveNoteTopAppBar(
+    isEditingMode: Boolean,
+    onBackClick: () -> Unit,
+    onSaveNoteClick: () -> Unit,
+    onOpenColorPickerClick: () -> Unit,
+    onDeleteNoteClick: () -> Unit
+) {
+
+}
+
+
+
 @Composable
 private fun ColorPicker(
     colors: List<ColorModel>,
